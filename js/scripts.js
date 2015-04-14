9aec5db1499b83d4e144621b462730316115d445
Jill Kuchman & James Nielson <student@epicodus.com>
2015-04-14 16:03:01 -0700

    Add dynamic status bar

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,27 @@ $(document).ready(function() {
     $('form#tamacrotchety-name').submit(function(event) {
         event.preventDefault();
 
-        var name = $('input#name').val();
+        var tamaName = $('input#name').val();
         var meds = 5;
         var grumbling = 5;
         var metamucil = 5;
         var nap = 5;
         var alive = true;
+        var medStatus = '';
+        var listenStatus = '';
+        var fiberStatus = '';
+        var napStatus = '';
 
         var tamacrotchety = {
 
+            tamaName: tamaName,
             meds: meds,
             grumbling: grumbling,
             metamucil: metamucil,
             nap: nap,
             alive: alive,
 
-            time = function() {
+            time: function() {
                 this.meds --;
                 this.grumbling ++;
                 this.metamucil --;
@@ -26,43 +31,51 @@ $(document).ready(function() {
                 this.isAlive();
             },
 
-            giveMeds = function() {
+            giveMeds: function() {
                 this.meds ++;
                 this.grumbling ++;
                 this.metamucil --;
                 this.nap --;
             },
 
-            listen = function() {
+            listen: function() {
                 this.meds --;
                 this.grumbling --;
                 this.metamucil --;
                 this.nap --;
             },
 
-            giveFiber = function() {
+            giveFiber: function() {
                 this.meds --;
                 this.grumbling ++;
                 this.metamucil ++;
                 this.nap --;
             },
 
-            giveNap = function() {
+            giveNap: function() {
                 this.meds --;
                 this.grumbling --;
                 this.metamucil --;
                 this.nap ++;
             },
 
-            isAlive = function() {
-            if(this.meds === 0 || this.meds > 10 || this.grumbling > 10 || this.metamucil === 0 || this.metamucil > 10 || this.nap === 0 || this.nap > 10) {
-                this.alive = false;
-            }
-        }
+            isAlive: function() {
+                if(this.meds === 0 || this.meds > 10 || this.grumbling > 10 || this.metamucil === 0 || this.metamucil > 10 || this.nap === 0 || this.nap > 10) {
+                    this.alive = false;
+                }
+            },
+
+
+
+        };
+        $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
+        $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
+        $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
+        $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
+
+        $("#tamaName").text(tamacrotchety.tamaName);
+        $("#tamacrotchety-form").hide();
+        $("#tamacrotchety-created").show();
 
     });
-    //controls
-    $('form#tamacrotchety-properties')
-
-
 });
