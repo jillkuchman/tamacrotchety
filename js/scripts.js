a73b078430e22393314ba8c2b72e532de6afa6cb
Jill Kuchman <jillkuchman@gmail.com>
2015-04-15 21:47:32 -0700

    Refactor status bar and death test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
 
+    var meds = 5;
+    var grumbling = 5;
+    var metamucil = 5;
+    var nap = 5;
+    var alive = true;   
+    var tamacrotchety;
+
+    function statusBar() {
+        $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
+        $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
+        $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
+        $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
+    }
+
+    function deathTest() {
+        if(!tamacrotchety.alive){
+            $("#dead").show();
+            $("#tamacrotchety-created").hide();
+            $("#death-message").text( tamacrotchety.tamaName + " has died.");
+        }
+    }
+    
     $('form#tamacrotchety-name').submit(function(event) {
         event.preventDefault();
 
         var tamaName = $('input#name').val();
-        var meds = 5;
-        var grumbling = 5;
-        var metamucil = 5;
-        var nap = 5;
-        var alive = true;
-        var tamacrotchety = {
-
+        tamacrotchety = {
             tamaName: tamaName,
             meds: meds,
             grumbling: grumbling,
@@ -61,17 +77,12 @@ $(document).ready(function() {
             isAlive: function() {
                 if(this.meds === 0 || this.meds > 10 || this.grumbling > 10 || this.metamucil === 0 || this.metamucil > 10 || this.nap === 0 || this.nap > 10) {
                     this.alive = false;
-
                 }
-            },
+            }
 
         };
 
-        $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
-        $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
-        $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
-        $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
-
+        statusBar();
 
         $("#tamaName").text(tamacrotchety.tamaName);
         $("#tamacrotchety-form").hide();
@@ -79,52 +90,26 @@ $(document).ready(function() {
 
         $('#give-meds').click(function(){
             tamacrotchety.giveMeds();
-            $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
-            $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
-            $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
-            $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
-            if(!tamacrotchety.alive){
-                $("#dead").show();
-                $("#tamacrotchety-created").hide();
-
-            }
+            statusBar();
+            deathTest();
         });
 
         $('#listen-to-grumbling').click(function(){
             tamacrotchety.listen();
-            $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
-            $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
-            $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
-            $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
-            if(!tamacrotchety.alive){
-                $("#dead").show();
-                $("#tamacrotchety-created").hide();
-
-            }
+            statusBar();
+            deathTest();
         });
 
         $('#give-fiber').click(function(){
             tamacrotchety.giveFiber();
-            $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
-            $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
-            $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
-            $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
-            if(!tamacrotchety.alive){
-                $("#dead").show();
-                $("#tamacrotchety-created").hide();
-            }
+            statusBar();
+            deathTest();
         });
 
         $('#give-nap').click(function() {
             tamacrotchety.giveNap();
-            $('#statusMeds').attr("style", "width: " + tamacrotchety.meds*10 + "%" );
-            $('#statusListen').attr("style", "width: " + tamacrotchety.grumbling*10 + "%" );
-            $('#statusFiber').attr("style", "width: " + tamacrotchety.metamucil*10 + "%" );
-            $('#statusNap').attr("style", "width: " + tamacrotchety.nap*10 + "%" );
-            if(!tamacrotchety.alive){
-                $("#dead").show();
-                $("#tamacrotchety-created").hide();
-            }
+            statusBar();
+            deathTest();
         });
 
 
